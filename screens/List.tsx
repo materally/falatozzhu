a3edd304ab5f7df0fb3a17f34937afc2f4771e38
Kulcsar Mate <kulcsarmate@gmail.com>
2023-02-10 14:23:40 +0100

    refactor: :recycle: rerender list when is focused

--- screens/List.tsx
+++ screens/List.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FlatList } from 'react-native';
+import { useIsFocused } from '@react-navigation/native'
 import { Card } from '../components/Card';
 import { useDb } from '../hooks/useDb';
 import Layout from '../ui/Layout';
-
+import { randomId } from '../utils/randomId';
 
 export const ListScreen = () => {
-  const { items } = useDb();
+  const { items, getItems } = useDb();
+  const isFocused = useIsFocused();
+
+  useEffect(() => {
+    if(isFocused) getItems();
+  }, [isFocused])
 
   return (
     <Layout>
@@ -14,7 +20,7 @@ export const ListScreen = () => {
         contentContainerStyle={{ alignItems: 'center', paddingTop: 10 }}
         data={items}
         renderItem={({ item }) => <Card item={item} />}
-        keyExtractor={item => item.id}
+        keyExtractor={item => item.id || randomId()}
       />
     </Layout>
   );
