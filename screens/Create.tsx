7d27a63757d8873145075f55e4339f9c3ca88d19
Kulcsar Mate <kulcsarmate@gmail.com>
2023-02-10 16:57:41 +0100

    refactor: :recycle: make common interface for Create and Edit screen

--- screens/Create.tsx
+++ screens/Create.tsx
@@ -1,81 +1,34 @@
-import React, { useState } from 'react';
-import { Alert, Button } from 'react-native';
+import React from 'react';
+import { Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { Input, InputValue } from '../components/Input';
+
 import { useDb } from '../hooks/useDb';
 import Layout from '../ui/Layout';
-import { validateInput } from '../utils/validateInput';
-import { numberInput } from '../utils/numberInput';
+import { useCreateEdit } from '../hooks/useCreateEdit';
 
 export const CreateScreen = () => {
-  const [name, setName] = useState<InputValue>('');
-  const [description, setDescription] = useState<InputValue>('');
-  const [quantity, setQuantity] = useState<InputValue>('');
+  const { name, setName, description, setDescription, quantity, setQuantity, validated, renderInputs } = useCreateEdit({});
+
   const { create } = useDb();
   const navigation = useNavigation();
   
   const createItem = () => {
-    let hasError = false;
+    if(validated()){
+      create({ name: String(name), description: String(description), quantity: Number(quantity) })
 
-    if(!validateInput(name)) {
-      setName(undefined);
-      hasError = true;
+      setName('');
+      setDescription('');
+      setQuantity('');
+      
+      navigation.navigate('List' as never);
     }
-
-    if(!validateInput(description)) {
-      setDescription(undefined);
-      hasError = true;
-    }
-
-    if(!validateInput(quantity)) {
-      setQuantity(undefined);
-      hasError = true;
-    }
-
-    if(hasError){
-      Alert.alert('Hiba! Minden mező kitöltése kötelező!');
-      return;
-    }
-
-    create({ name: String(name), description: String(description), quantity: Number(quantity) })
-
-    setName('');
-    setDescription('');
-    setQuantity('');
-    
-    navigation.navigate('List' as never);
   }
 
   return (
     <Layout>
+      { renderInputs() }
 
-      <Input 
-        title="Név"
-        value={name} 
-        onChangeText={setName} 
-        placeholder="Név"
-        error={name === undefined}
-      />
-
-      <Input 
-        title="Rövid leírás"
-        value={description} 
-        onChangeText={setDescription} 
-        placeholder="Rövid leírás"
-        error={description === undefined}
-      />
-
-      <Input
-        keyboardType="number-pad"
-        title="Mennyiség"
-        value={quantity} 
-        onChangeText={(value) => numberInput({value, setter: setQuantity})} 
-        placeholder="Mennyiség"
-        error={quantity === undefined}
-      />
-      
       <Button title="Mentés" onPress={createItem} />
-      
     </Layout>
   );
 }
