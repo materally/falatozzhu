26e8cf773f9ef8470904f997bd164b4803e723ce
Kulcsar Mate <kulcsarmate@gmail.com>
2023-02-10 17:53:46 +0100

    refactor: :recycle: use .env

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,3 +1,3 @@
-import { API_URL } from "../consts";
+import { API_URL } from "@env";
 
 export const fetcher = (url: string, options: {} = {}) => fetch(API_URL + url, options).then((res) => res.json()).then(res => res)
